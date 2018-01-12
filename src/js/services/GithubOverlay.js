87f2d4b3e739c75fec6523e1c22456181eabfd82
Jordan Oroshiba <jordan@oroshiba.me>
2018-01-12 12:59:35 -0600

    Copy SVG info locally

--- src/js/services/GithubOverlay.js
+++ src/js/services/GithubOverlay.js
@@ -1,6 +1,7 @@
 // @flow
 import * as $ from 'jquery';
 import type { IOverlay } from './OverlayInterface';
+import overlayHelper from '../helpers/overlayHelper';
 
 export default class GitHubOverlay implements IOverlay {
   sha: ?string
@@ -90,13 +91,16 @@ export default class GitHubOverlay implements IOverlay {
 
     if (textForRow === '*') {
       const commitTease = $('.commit-tease .float-right');
-      commitTease.prepend(`<img class="coveralls coveralls-percent-badge" 
-                                style="position: absolute; right: ${commitTease.width() + 10}px;" 
-                                src="https://s3.amazonaws.com/assets.coveralls.io/badges/coveralls_${Math.round(coverage.paths_covered_percent)}.svg">`);
+      commitTease.prepend(`<span class="coveralls coveralls-percent-badge" 
+                                 style="position: absolute; right: ${commitTease.width() + 10}px;">
+                             ${overlayHelper.svgBadge(Math.round(coverage.paths_covered_percent))}
+                           </span>`);
     } else {
       $(`tr.js-navigation-item:contains(${textForRow})`).each(function addCoverageInformation() {
         if ($(this).find('.content .js-navigation-open')[0].innerText === textForRow) {
-          $(this).find('td:last').prepend(`<img class="coveralls coveralls-percent-badge" src="https://s3.amazonaws.com/assets.coveralls.io/badges/coveralls_${Math.round(coverage.paths_covered_percent)}.svg">`);
+          $(this).find('td:last').prepend(`<span class="coveralls coveralls-percent-badge">
+                                             ${overlayHelper.svgBadge(Math.round(coverage.paths_covered_percent))}
+                                           </span>`);
         }
       });
     }
